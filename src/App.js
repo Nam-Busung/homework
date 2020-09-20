0cf3ef90be7fe12417f6ea4969843cdb83c515dc
Nam-Busung <46341567+Nam-Busung@users.noreply.github.com>
2020-09-20 15:02:30 +0900

    homework_complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Heading } from './components/Heading';
 
 import { UnsplashImage } from './components/UnsplashImage';
 import { Loader } from './components/Loader';
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
-
+import ic_check from './img/checked.png';
+import ic_uncheck from './img/uncheck.png';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from 'axios';
-let i=0;
+import useStateWithCallback from 'use-state-with-callback';
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-  }
+let i = 1;
+
+const Header = styled.header`
+  max-width: 72rem;
+  margin: 2rem auto;
+  margin-bottom: -17px;
 `;
+
 const WrapperImages = styled.section`
   max-width: 75rem;
   margin: 5rem auto;
@@ -26,47 +28,126 @@ const WrapperImages = styled.section`
 
 function App() {
   const [images, setImage] = useState([]);
+  const [checker, setChecker] = useState(ic_uncheck);
+  const [hasmore,setHasmore] = useState(true);
+  const Heading = () => {
+    return (
+      <Header>
+        <a onClick={() => {
+          if (checker == ic_uncheck) {
+
+            setChecker(ic_check)
+            localStorage.setItem(
+              'checker',
+              JSON.stringify(ic_check)
+            )
+            window.location.reload()
+
+          }
+          else {
+            setChecker(ic_uncheck)
+            localStorage.setItem(
+              'checker',
+              JSON.stringify(ic_uncheck)
+            )
+            window.location.reload()
+
+          }
+        }} >
+          <img style={{ marginTop: "-3px", cursor: "pointer", position: "absolute", width: "24px", height: "24px" }} src={checker}>
+          </img>
+        </a>
+        <p style={{ marginLeft: "31px", fontFamily: "AppleSDGothicNeo-Regular", fontSize: "15px" }}>스크랩 한 것만 보기</p>
+      </Header>
+    )
+  }
 
   useEffect(() => {
-    fetchImages();
+
+    if (localStorage.getItem('checker') != null) {
+      setChecker(JSON.parse(localStorage.getItem('checker')));
+    } else {
+      localStorage.setItem(
+        'checker',
+        JSON.stringify(ic_uncheck)
+      )
+    }
+
+    if (JSON.parse(localStorage.getItem('checker')) == ic_uncheck) {
+      fetchImages();
+    } else {
+      scrapFetchImages();
+    }
+
+
   }, [])
 
-  const fetchImages = () => {
-    
+  const scrapFetchImages = () => {
+    setHasmore(false);
+
+    let ary = new Array();
+    console.log(i);
     setTimeout(() => {
+      for(let k=1; k<6;k++){
+        axios
+        .get(`https://bucketplace-coding-test.s3.amazonaws.com/cards/page_${k}.json`)
+        .then(res => {
+          for(let j=0;j<res.data.length; j++){
+            if (localStorage.getItem('scrap') != null) {
+              if (JSON.parse(localStorage.getItem('scrap')).indexOf(res.data[j].image_url) != -1) {
+                ary.push(res.data[j])
+                setImage([...images, ...ary]);
+              }
+            }
+          }
+        })
+      }
+
       console.log(i);
+
+    }, 1000)
+
+  }
+  const fetchImages = () => {
+    if(i==6)
+      setHasmore(false);
+    setTimeout(() => {
 
       axios
         .get(`https://bucketplace-coding-test.s3.amazonaws.com/cards/page_${i}.json`)
         .then(res => {
-          setImage([...images, ...res.data]);
-      })
-    }, 1500);
-  
-    i++;
+          setImage([...images, ...res.data]);          
+        })
+      i++;
+    }, 1000)
 
   }
 
   return (
-
-    <div  className="container">
-      <Heading/>
+    <div className="container">
+      <Heading />
       <InfiniteScroll
         dataLength={images.length}
-        next={fetchImages}
-        hasMore={i<5}
-
+        next={() => {
+          if (JSON.parse(localStorage.getItem('checker')) == ic_uncheck) {
+            fetchImages();
+          }else {
+            scrapFetchImages();
+          }
+        }
+        }
+        hasMore={
+          hasmore
+        }
         loader={<Loader />}
       >
-        
         <WrapperImages>
-
           {
-          images.map(image => (
-            <>
-            <UnsplashImage url={image.image_url} profile={image.profile_image_url} nick={image.nickname}/>
-            </>
-          ))
+            images.map(image => (
+              <>
+                <UnsplashImage url={image.image_url} profile={image.profile_image_url} nick={image.nickname} />
+              </>
+            ))
           }
         </WrapperImages>
       </InfiniteScroll>
--- src/components/UnsplashImage.js
+++ src/components/UnsplashImage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import scrap from '../img/on-img.png'
 import blue from '../img/blue.png'
@@ -13,28 +13,48 @@ const Img = styled.img`
   margin-top:0px;
   `;
 
-export const UnsplashImage = ({ url, profile,nick }) => {
+export const UnsplashImage = ({ url, profile, nick }) => {
   const [images, setImage] = useState(scrap);
-
+  useEffect(() => {
+    if (localStorage.getItem('scrap') != null) {
+      if (JSON.parse(localStorage.getItem('scrap')).indexOf(url) != -1) {
+        setImage(blue);
+      }
+    }
+  }, [])
   return (
     <>
-
-      <Img  src={url} alt="" >
-        
+      <Img src={url} alt="">
       </Img>
-
       <a onClick={() => {
-        if (images == scrap)
-          setImage(blue)
-        else
-          setImage(scrap)
+        if (images == scrap) {
+          setImage(blue);
+
+          if (localStorage.getItem('scrap') != null) {
+            localStorage.setItem(
+              'scrap',
+              JSON.stringify(JSON.parse(localStorage.getItem('scrap')) + '||' + url)
+            )
+          } else {
+            localStorage.setItem(
+              'scrap',
+              JSON.stringify('||' + url)
+            )
+          }
+        }
+        else {
+          setImage(scrap);
+          localStorage.setItem(
+            'scrap',
+            JSON.stringify(JSON.parse(localStorage.getItem('scrap')).replace('||' + url, ""))
+            )
+        }
       }} style={{ cursor: "pointer", position: "absolute", width: "30px", height: "30px", marginTop: "228px", marginLeft: "-40px" }}>
         <img style={{ position: "absolute", width: "32px", height: "32px" }} src={images}>
         </img>
       </a>
-      <Avatar  size="36" style={{position: "absolute",marginLeft:"-268px",marginTop:"-46px"}}  src={profile} ></Avatar>
-      <text style={{fontFamily:"AppleSDGothicNeo-Bold",fontSize:"15px",position: "absolute",marginLeft:"-222px",marginTop:"-36px"}}>{nick}</text>
-
+      <Avatar size="36" style={{ position: "absolute", marginLeft: "-268px", marginTop: "-46px" }} src={profile} ></Avatar>
+      <text style={{ fontFamily: "AppleSDGothicNeo-Bold", fontSize: "15px", position: "absolute", marginLeft: "-222px", marginTop: "-36px" }}>{nick}</text>
     </>
   )
 }
